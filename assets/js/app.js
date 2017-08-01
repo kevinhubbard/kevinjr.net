dbdc7e00b0c778aeebce33ea39efbb299334b991
Kevin Hubbard <dgkallday98@gmail.com>
2017-07-31 20:41:40 -0400

    updated js/db queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,23 @@
 $(document).ready(function() {
 
-/*$('#card').flip('toggle');*/
-
-
-
-
-
-
-
 var $nameInput = $('#nameInput');
 var $emailInput = $('#emailInput');
 var $msgInput = $('#msgInput');
 
-  var config = {
-    apiKey: "AIzaSyDICfc-6WmELtRbTB5DSg9j67PAXvWQNkA",
-    authDomain: "portfolio-558a1.firebaseapp.com",
-    databaseURL: "https://portfolio-558a1.firebaseio.com",
-    storageBucket: "portfolio-558a1.appspot.com",
-    messagingSenderId: "28937066118"
-  };
-  firebase.initializeApp(config);
 
-var data = firebase.database();
-
-
-$(document).on('click', '.btn', function () {
-	var user = {
-		name: $nameInput.val(),
-		email: $emailInput.val(),
-		msg: $msgInput.val()
-	}
-	
-	var json = JSON.stringify(user);
-
-	data.ref('users/' + user.name).set(user);
-
-	clearUser();
-});
 
 function clearUser() {
 	$nameInput.val('');
 	$emailInput.val('');
 	$msgInput.val('');
 }
+
+//Flip animation for images
+$(".card").flip({
+  axis: 'x',
+  trigger: 'hover'
+});
+console.log('app.js loaded');
 
 
 });
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser')
 
 //DEFINES APP METHOD
 var app = express();
@@ -13,6 +15,36 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 //DEFINES PORT
 var PORT = process.env.PORT || 8080;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+//SETS UP DB CONNECTION
+var URI = 'mongodb://heroku_3pj31sf1:73t6prtueeq1a9c40ljtit5s31@ds127883.mlab.com:27883/heroku_3pj31sf1';
+
+
+
+
+//TEST POST
+app.post('/thankyou', urlencodedParser, function(req, res){
+
+	console.log(req.body.name);
+	console.log(req.body.email);
+	console.log(req.body.msg);
+	console.log(req.body.role);
+
+//LOG USER INPUT TO DATABASE
+MongoClient.connect(URI, function(err, db){
+	if(err){
+		console.log(err);
+	} else {
+		var userInput = db.collection('userInput');
+		userInput.insertOne({name: req.body.name, email: req.body.email, msg: req.body.msg, role: req.body.role});
+		console.log('successfully submitted data to MONGODB');
+	}
+});
+	
+	res.render('thankyou');
+});
 
 //SETS HANDLEBARS AS OUR MAIN VIEW ENGINE AND USES MAIN AS DEFAULT LAYOUT
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
