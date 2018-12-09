ea79f21faf3e5b89398a6ee48044e5ebe0b9f936
Kevin Hubbard <dgkallday98@gmail.com>
2018-12-09 17:37:18 -0500

    working on server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	res.sendFile('index');
