6dfa2ebd7704a81d578c5d2678fbbfea2b23938d
Kevin Hubbard <dgkallday98@gmail.com>
2019-05-04 22:36:46 -0400

    updated app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,15 @@
 $(document).ready(function() {
 
-var $nameInput = $('#nameInput');
-var $emailInput = $('#emailInput');
-var $msgInput = $('#msgInput');
-var $foot = $('#foot');
-var year = new Date().getFullYear();
-
-$foot.append(year);
+	var $foot = $('#foot');
+	var year = new Date().getFullYear();
+	$foot.append(year);
 
 
-function clearUser() {
-	$nameInput.val('');
-	$emailInput.val('');
-	$msgInput.val('');
-}
-
-//Flip animation for images
-$(".card").flip({
-  axis: 'x',
-  trigger: 'hover'
-});
-
+	//Flip animation for images
+	$(".card").flip({
+	  axis: 'x',
+	  trigger: 'hover'
+	});
 
 });
 
