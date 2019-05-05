bfe05f17c77b37a1c7a2fe8d6521a1ccbb569a4f
Kevin Hubbard <dgkallday98@gmail.com>
2019-05-04 22:35:51 -0400

    updated server

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
-//PACKAGES REQUIRED FOR EXPRESS SERVER AND HANDLEBARS TEMPLATING 
+// PACKAGES REQUIRED
 var express = require('express');
 var exphbs = require('express-handlebars');
 var expressRobotsMiddleware = require('express-robots-middleware');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-//Require routes
-var index = require('./routes/index');
-var admin = require('./routes/admin');
-var blog = require('./routes/blog');
-var contact = require('./routes/contact');
-var portfolio = require('./routes/portfolio');
-var resume = require('./routes/resume');
-var thankyou = require('./routes/thankyou');
-
-//DEFINES APP METHOD
+// DEFINES APP METHOD
 var app = express();
 
-//LETS EXPRESS USE STATIC FILES
+// LETS EXPRESS USE STATIC FILES
 app.use(express.static('node_modules'));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//DEFINES PORT
+// DEFINES PORT
 var PORT = process.env.PORT || 3000;
 
 //SETS HANDLEBARS AS OUR MAIN VIEW ENGINE AND USES MAIN AS DEFAULT LAYOUT
@@ -41,22 +32,22 @@ var robotsMiddleware = expressRobotsMiddleware([{
 // ROBOTS.TXT ROUTE
 app.get('/robots.txt', robotsMiddleware);
 
-//Use routes
-app.use('/', index);
-app.use('/admin', admin);
-app.use('/blog', blog);
-app.use('/contact', contact);
-app.use('/portfolio', portfolio);
-app.use('/resume', resume);
-app.use('/thankyou', thankyou);
+// ROUTES
+app.use('/', require('./routes/index'));
+app.use('/admin', require('./routes/admin'));
+app.use('/blog', require('./routes/blog'));
+app.use('/contact', require('./routes/contact'));
+app.use('/portfolio', require('./routes/portfolio'));
+app.use('/resume', require('./routes/resume'));
+app.use('/thankyou', require('./routes/thankyou'));
 
 
-//404 CATCH FOR PORTFOLIO
+// 404 CATCH
 app.use(function (req, res, next) {
 	res.status(404).render('404');
 });
 
-//START SERVER LISTENING
+// START SERVER LISTENING
 app.listen(PORT, function () {
 	console.log('App listening on port: ' + PORT);
 });
