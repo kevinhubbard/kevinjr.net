51c571d4855960e3d4d7207fa639640a54aa1a53
Kevin H <dgkallday98@gmail.com>
2022-02-08 08:53:17 -0500

    updated info model

--- models/info.js
+++ models/info.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var infoSchema = new Schema({
+var visitorSchema = new Schema({
 	name: String,
 	number: Number
 });
 
-var Info = mongoose.model('Info', infoSchema);
+var Visitor = mongoose.model('Visit', visitorSchema);
 
-module.exports = Info;
+module.exports = Visitor;
