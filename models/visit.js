7795d0d6964830320a2c22bee330eaea3c80af9e
Kevin H <dgkallday98@gmail.com>
2022-02-15 19:46:22 -0500

    updated models

---
+++ models/visit.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var visitorSchema = new Schema({
+	name: String,
+	number: Number
+});
+
+var Visitor = mongoose.model('Visit', visitorSchema);
+
+module.exports = Visitor;
