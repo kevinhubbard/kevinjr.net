dfd4ffc2a718817db5a73a7d8d71ac47ee8379fe
Kevin H <dgkallday98@gmail.com>
2023-07-03 16:21:21 -0400

    fetch updates

--- assets/js/botw.js
+++ assets/js/botw.js
@@ -1,1 +1,16 @@
-console.log('comon dude.');
+window.addEventListener("DOMContentLoaded", (event) => {
+async function logJSONData() {
+	const URL = "http://kevinjr.net/botw/data"
+  const response = await fetch(URL);
+  console.log(response);
+  const jsonData = await response.json();
+  for(var i = 0; i<jsonData.length; i++){
+  	let thing = document.createElement('p');
+  	thing.innerText = jsonData[i].ingredientName;
+  	document.getElementById('farts').appendChild(thing);
+  }
+}
+
+logJSONData();
+
+});
