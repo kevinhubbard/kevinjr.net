bc835272693784313f6f3371174ac7f76e871358
Kevin H <dgkallday98@gmail.com>
2023-07-07 15:46:19 -0400

    golfcard updates

--- assets/js/golfScript.js
+++ assets/js/golfScript.js
@@ -2,25 +2,26 @@ window.addEventListener("DOMContentLoaded", function(event) {
 	var newRoundBtn = document.getElementById('newRound');
 	var lookupRoundBtn = document.getElementById('lookupRound');
 	var opt = document.getElementById('options');
+	var startRound = document.getElementById('startRound');
 
 	newRoundBtn.addEventListener("click", function(){
-		console.log('start a new round function');
-		newRound('twisted dunes', '18');
+		opt.style.display = 'none';
+		document.getElementById('gamePanel').style.display = 'block';
 	});
 
 	lookupRoundBtn.addEventListener("click", function(){
-		console.log("lookup round function");
-		lookupRound('twisted dune');
-	});
-
-	function newRound() {
-		opt.style.display = 'none';
-		document.getElementById('startGame').style.display = 'block';
-	}
-
-	function lookupRound() {
 		opt.style.display = 'none';
 		document.getElementById('showRound').style.display = 'block';
-	}
+	});
+
+	startRound.addEventListener("click", function(){
+		console.log('starting round');
+		var courseName = document.getElementById('courseName').value;
+		//var holes = document.getElementsByName('holes').value;
+		console.log(courseName);
+		//console.log(holes);
+	});
+
+
 
 });
