071c33dd920be3c211f380c96c7fc6d2d002cd6f
Kevin H <dgkallday98@gmail.com>
2023-07-07 15:11:15 -0400

    golfcard updates

--- assets/js/golfScript.js
+++ assets/js/golfScript.js
@@ -1,1 +1,26 @@
-console.log("fuck");
+window.addEventListener("DOMContentLoaded", function(event) {
+	var newRoundBtn = document.getElementById('newRound');
+	var lookupRoundBtn = document.getElementById('lookupRound');
+	var opt = document.getElementById('options');
+
+	newRoundBtn.addEventListener("click", function(){
+		console.log('start a new round function');
+		newRound('twisted dunes', '18');
+	});
+
+	lookupRoundBtn.addEventListener("click", function(){
+		console.log("lookup round function");
+		lookupRound('twisted dune');
+	});
+
+	function newRound() {
+		opt.style.display = 'none';
+		document.getElementById('startGame').style.display = 'block';
+	}
+
+	function lookupRound() {
+		opt.style.display = 'none';
+		document.getElementById('showRound').style.display = 'block';
+	}
+
+});
