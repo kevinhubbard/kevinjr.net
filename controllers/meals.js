b46866e9908d412eaf004aed4bb02610e6c87571
Kevin H <dgkallday98@gmail.com>
2024-04-24 21:12:05 -0400

    updated controller to handle recored updates and deletes

--- controllers/meals.js
+++ controllers/meals.js
@@ -59,4 +59,39 @@ router.post('/', async function(req, res) {
 	});
 });
 
+router.post('/iup', async function(req, res) {
+	const info = req.body;
+
+	await Ingredient.update(
+	{	
+		ingredientName: info.ingredientName,
+		foodGroup: info.foodGroup,
+		servingSize: info.servingSize,
+		calories: info.calories,
+		totalFat: info.totalFat,
+		saturatedFat: info.saturatedFat,
+		transFat: info.transFat,
+		cholesterol: info.cholesterol,
+		sodium: info.sodium,
+		totalCarbohydrates: info.totalCarbohydrates,
+		fiber: info.fiber,
+		sugar: info.sugar,
+		protein: info.protein,
+		imgLocation: '/images/foods/someshit.jpg'
+	},
+		{where: 
+			{ingredientID: info.id}
+		}
+	);
+});
+
+router.delete('/delete', async function(req, res) {
+	const info = req.body;
+	await Ingredient.destroy({
+		where: {
+			ingredientID: info.id
+		}
+	});
+});
+
 module.exports = router;
