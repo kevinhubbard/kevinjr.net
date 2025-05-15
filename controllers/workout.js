da2fd96835ce56746ee25471247144c01ab18d61
Kevin H <dgkallday98@gmail.com>
2025-05-15 11:29:45 -0400

    updated workout controller to add api

--- controllers/workout.js
+++ controllers/workout.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var {MuscleGroup, Workout, Muscle} = require('../models/workouts.js');
+const { QueryTypes } = require('sequelize');
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+	//const group = await MuscleGroup.findAll();
+
 	res.render('workout', {
 		css: ['style.css', 'workout.css'],
-		js: ['menu.js']
+		js: ['menu.js', 'workout.js']
 	});
+});
+
+router.get('/wapi', async function(req, res) {
+	const wrkt = await Workout.findAll();
+	res.json(wrkt);
 });
 
 module.exports = router;
