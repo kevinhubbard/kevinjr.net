1445ec83436b1419d4620a014dbbe19d4ac13a21
Kevin H <dgkallday98@gmail.com>
2025-06-02 14:14:00 -0400

    updated db connections to use new variables

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'production') {
 		dialect: 'mysql'
 	});
 } else {
-	var db = require('../config');
+	var db = require('../config').db;
 	connection = new Sequelize(db.DATABASE, db.USER, db.PASSWORD, {
 		host: db.HOST,
 		dialect: 'mysql',
