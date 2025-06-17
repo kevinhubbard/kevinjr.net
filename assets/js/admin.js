5fe5b952d7877a48a6bfdcdfe38ccb330cf5b81c
Kevin H <dgkallday98@gmail.com>
2025-06-16 22:37:55 -0400

    fetch posts images to assets folder

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,38 @@
 window.addEventListener('DOMContentLoaded', function(event) {
-	console.log("admin page");
+
+document.querySelector('input[type="file"]').addEventListener('change', function(e) {
+	const fileInput = this;
+	const formData = new FormData();
+	formData.append('image', fileInput.files[0]);
+
+	fetch('/admin/upload', {
+		method: 'POST',
+		body: formData
+	})
+	.then(res => res.json())
+	.then(data => {
+		if (data.success) {
+			const textarea = document.getElementById('postBody');
+			const imgTag = `<img src="${data.imageUrl}" alt="uploaded img">\n`;
+			textarea.value += imgTag;
+		}
+	})
+	.catch(err => console.error('upload failed:', err));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*	console.log("admin page");
 
 	document.getElementById('submitBtn').addEventListener('click', function(e) {
 		e.preventDefault();
@@ -22,5 +55,5 @@ window.addEventListener('DOMContentLoaded', function(event) {
 			},
 			body: JSON.stringify({t: titleVar, a: authVar, v: bodyVar}),
 		})//.then(response => response.text()).then(data => console.log(data));
-	}
+	}*/
 });
