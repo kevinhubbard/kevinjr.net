e18273ea1142eea33fab11d807bfbe324d58a63e
Kevin H <dgkallday98@gmail.com>
2025-06-18 23:37:35 -0400

    removed old code

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -20,40 +20,4 @@ document.querySelector('input[type="file"]').addEventListener('change', function
 	.catch(err => console.error('upload failed:', err));
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*	console.log("admin page");
-
-	document.getElementById('submitBtn').addEventListener('click', function(e) {
-		e.preventDefault();
-		const titleVar = document.getElementById('postTitle').value;
-		const bodyVar = document.getElementById('postBody').value;
-		const authVar = document.getElementById('postAuth').value;
-		//console.log('title: ' + titleVar + '\nbody: ' + bodyVar + '\nauth: ' + authVar);
-		savePost()
-	});
-
-	function savePost() {
-		const titleVar = document.getElementById('postTitle').value;
-		const bodyVar = document.getElementById('postBody').value;
-		const authVar = document.getElementById('postAuth').value;
-
-		fetch('/admin/blog-post', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({t: titleVar, a: authVar, v: bodyVar}),
-		})//.then(response => response.text()).then(data => console.log(data));
-	}*/
 });
