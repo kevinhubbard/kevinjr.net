83be6a09ac02f9b3c2c8754d542658fcf69f02b6
Kevin H <dgkallday98@gmail.com>
2025-06-23 11:46:46 -0400

    updated contact controller

--- controllers/thankyou.js
+++ controllers/thankyou.js
@@ -1,24 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-var Contact = require('../models/contact.js');
+const express = require('express');
+const router = express.Router();
+const Contact = require('../models/contact.js');
 
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
-
-//POST USER INFO FROM CONTACT PAGE
-router.post('/', urlencodedParser, function(req, res){
-
-	var info = new Contact({
-		name: req.body.name,
-		email: req.body.email,
-		msg: req.body.msg,
-		role: req.body.role,
-		state: req.body.state,
-		date: Date.now()
-	}); 
-
-	if(info.state) {
+router.post('/',  async function(req, res){
+	if(req.body.state) {
 		res.render('thankyou', {
 			name: req.body.name,
 			email: req.body.email,
@@ -26,22 +11,14 @@ router.post('/', urlencodedParser, function(req, res){
 			js: ['menu.js']
 		});
 	} else {
-		info.save(function(err){
-			if (err) return handleError(err);
-			res.render('thankyou', {
-				name: req.body.name, 
-				email: req.body.email, 
-				css: ['style.css'],
-				js: ['menu.js']
-			});
-		});
-
+		const msg = await Contact.create({name: req.body.name, email: req.body.email, message: req.body.msg});
+		res.render('thankyou', {
+			name: req.body.name,
+			email: req.body.email,
+			css: ['style.css'],
+			js: ['menu.js']
+		})
 	}
-
-
-
-
-
 });
 
 module.exports = router;
