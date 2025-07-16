0e965498cfea763d8a512ef5d8314412ebbc9f04
Kevin H <dgkallday98@gmail.com>
2025-07-16 13:42:50 -0400

    integrated /thankyou route into /contact route

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Contact = require('../models/contact.js');
+const User = require('../models/user.js');
 
 //contact route
 router.get('/', function(req, res){
@@ -7,6 +9,27 @@ router.get('/', function(req, res){
 		css: ['style.css', 'contact.css'],
 		js: ['menu.js', 'loginScript.js']
 	});
+});
+
+router.post('/',  async function(req, res){
+
+	const user = await User.findOne({where:{email: req.body.email}});
+	if (user) {
+		const msg = await Contact.create({name: user.name, email: user.email, message: req.body.msg});
+		return res.render('contact', {
+			success: true,
+			message: `Thank you ${user.name}, I have received your information and will respond to you at ${user.email} shortly…`,
+			css: ['style.css', 'contact.css'],
+			js: ['menu.js', 'loginScript.js']
+		})
+	} else {
+		return res.render('contact', {
+			message: 'You must have a verified email to send me a message. Please re-enter your email or create an account first.',
+			success: false,
+			css: ['style.css', 'contact.css'],
+			js: ['menu.js', 'loginScript.js']
+		});
+	}
 });
 
 module.exports = router;
