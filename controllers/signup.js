88337a8a429c510a922ac8b968543b109a19507c
Kevin H <dgkallday98@gmail.com>
2025-07-16 11:36:46 -0400

    added recaptcha for site signup

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 //GETS RESUME ROUTE
 router.get('/', function (req, res) {
@@ -12,9 +14,43 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', async function (req, res) {
+
+	// recaptcha test
+	const secretKey = process.env.RE_CAPTCHA_SK;
+	const recaptchaToken = req.body['g-recaptcha-response'];
 	const uuid = crypto.randomUUID();
 	const email = req.body.userEmail;
+
+	if (!recaptchaToken) {
+		return res.render('signup', {
+			message: "Please complete the reCAPTCHA.",
+			success: false,
+			css: ['style.css'],
+			js: ['menu.js', 'createUser.js', 'loginScript.js']
+		});
+	}
+
 	try {
+
+		const verifyRecaptcha = async (secret, token) => {
+			const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: `secret=${secret}&response=${token}`
+			});
+		return response.json();
+		};
+
+		const recaptchaRes = await verifyRecaptcha(secretKey, recaptchaToken);
+		if (!recaptchaRes.success) {
+			return res.render('signup', {
+				message: 'reCAPTCHA verification failed. Please try again.',
+				success: false,
+				css: ['style.css'],
+				js: ['menu.js', 'createUser.js', 'loginScript.js']
+			});
+		}
+
 		const user = await User.findOne({where:{email}});
 		if (user) {
 			return res.render('signup', {
