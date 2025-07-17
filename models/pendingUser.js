53d32d00412b9ba48b614d680ed98ae9bde9aaff
Kevin H <dgkallday98@gmail.com>
2025-07-17 16:15:36 -0400

    added a new pending user model

---
+++ models/pendingUser.js
@@ -0,0 +1,45 @@
+const {Sequelize, DataTypes, QueryTypes} = require('sequelize');
+var sequelize = require('../database/dbConnection');
+
+const PendingUser = sequelize.define('PendingUser', { 
+	publicID: {
+		type: DataTypes.STRING(36),
+		primaryKey: true,
+		unique: true,
+		allowNull: false
+	},
+	email: {
+		type: DataTypes.STRING(45),
+		unique: true,
+		allowNull: false
+	},
+	name: {
+		type: DataTypes.STRING(30),
+		allowNull: false
+	}, 
+	password: {
+		type: DataTypes.STRING(60),
+		allowNull: false
+	},
+	role: {
+		type: DataTypes.ENUM,
+		values: ['admin', 'editor', 'user'],
+		defaultValue: 'user',
+		allowNull: false
+	},
+	token: {
+		type: DataTypes.STRING(64),
+		allowNull: false
+	},
+	expiresAt: {
+		type: DataTypes.DATE,
+		allowNull: false
+	}
+}, {
+	tableName: 'PendingUsers',
+	timestamps: false,
+	createdAt: false,
+	updatedAt: false
+});
+
+module.exports = PendingUser;
