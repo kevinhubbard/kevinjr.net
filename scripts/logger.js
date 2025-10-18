a4a2e6cf7b4028178ce94d511675785313303aa7
Kevin H <dgkallday98@gmail.com>
2025-10-18 12:47:05 -0400

    added res.statusCode to log file and added two log routes banned/allowed log

--- scripts/logger.js
+++ scripts/logger.js
@@ -1,19 +1,28 @@
 const fs = require('fs');
 const path = require('path');
-
-const logPath = path.join(__dirname, '../logs/visitors.log');
+let logPath = '';
 
 function logVisitor(req, res, next) {
-	const logEntry = [
-		new Date().toISOString(),
-		req.ip,
-		req.method,
-		req.originalUrl,
-		req.headers['user-agent'] || 'Unknown'
-	].join(' | ') + '\n';
+	const time = new Date();
+	res.on('finish', function() {
+		const logEntry = [
+			time.toISOString(),
+			req.headers['cf-connecting-ip'] || req.ip,
+			req.method,
+			req.originalUrl,
+			res.statusCode,
+			req.headers['user-agent'] || 'Unknown'
+		].join(' | ') + '\n';	
 
-	fs.appendFile(logPath, logEntry, err => {
-		if (err) console.error('Error writing visitor log: ', err);
+		if (res.statusCode === 403) {
+			logPath = path.join(__dirname, '../logs/bannedVisitor.log');
+		} else {
+			logPath = path.join(__dirname, '../logs/allowedVisitor.log');
+		}
+
+		fs.appendFile(logPath, logEntry, function(err) {
+			if (err) console.error('Error writing visitor log: ', err);
+		});
 	});
 
 	next();
