041d288a59c605df99855196f4b46a0ca802c917
Kevin H <dgkallday98@gmail.com>
2025-10-26 14:47:10 -0400

    db updates

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,26 +1,42 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-let connection;
 
-if (process.env.NODE_ENV === 'production') {
-	connection = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-		host: process.env.DB_HOST,
-		dialect: 'mysql',
-		logging: false
-	});
+let isProd = process.env.NODE_ENV === 'production';
+let poolConfig;
+
+if (isProd) {
+	poolConfig = {
+      	max: 10,			// maximum number of connections
+      	min: 0,				// minimum number of connections
+      	acquire: 30000,		// max time (ms) to try to get connection before throwing error
+      	idle: 10000			// time (ms) after which an idle connection is released
+	};
 } else {
-	connection = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-		host: process.env.DB_HOST,
-		dialect: 'mysql',
-		logging: false
-	});
+	poolConfig = {
+      	max: 3,
+      	min: 0,
+      	acquire: 10000,
+      	idle: 5000	
+	}
 }
+
+const connection = new Sequelize(
+	process.env.DB,
+	process.env.DB_USER,
+  	process.env.DB_PASSWORD,
+  	{
+    	host: process.env.DB_HOST,
+    	dialect: 'mysql',
+   		logging: !isProd,
+    	pool: poolConfig
+  	}
+);
 
 (async () => {
 	try {
-		connection.authenticate();
+		await connection.authenticate();
 		console.log('Connection successfully established.');
-		connection.sync();
+		await connection.sync();
 	} catch (error) {
 		console.error('Unable to connect to database', error);
 	}
