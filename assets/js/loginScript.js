9c4feaca7c762a45a07673dbb0cb6b179b3c205b
Kevin H <dgkallday98@gmail.com>
2025-11-23 18:47:38 -0500

    updated login script

--- assets/js/loginScript.js
+++ assets/js/loginScript.js
@@ -1,12 +1,42 @@
-window.addEventListener("DOMContentLoaded", function(event) {
-	let login = document.getElementById('logLink');
-	let form = document.getElementById('loginModel');
+// window.addEventListener("DOMContentLoaded", function(event) {
+// 	let login = document.getElementById('logLink');
+// 	let form = document.getElementById('loginModel');
 
-	document.getElementById('closeLogin').addEventListener('click', function(e) {
-		document.getElementById('loginModel').style.display = 'none';
-	})
+// 	document.getElementById('closeLogin').addEventListener('click', function(e) {
+// 		document.getElementById('loginModel').style.display = 'none';
+// 	})
 	
-	login.addEventListener('click', function(e) {
-		document.getElementById('loginModel').style.display = 'inline-block';
-	});
+// 	login.addEventListener('click', function(e) {
+// 		document.getElementById('loginModel').style.display = 'inline-block';
+// 	});
+// });
+
+
+
+window.addEventListener("DOMContentLoaded", function(event) {
+    let login = document.getElementById('logLink');
+    let form = document.getElementById('loginModel');
+    let overlay = document.getElementById('overlay');
+
+    // OPEN MODAL
+    login.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.add("active");
+        overlay.classList.add("active");
+    });
+
+    // PREVENT overlay click from closing immediately
+    form.addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+
+    // CLOSE WHEN CLICKING OUTSIDE
+    overlay.addEventListener('click', function(e) {
+        closeModal();
+    });
+
+    function closeModal() {
+        form.classList.remove("active");
+        overlay.classList.remove("active");
+    }
 });
