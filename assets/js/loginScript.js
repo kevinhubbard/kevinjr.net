eca42fdd56c417ac9ac0e4a1eb345add7fa6c41e
Kevin H <dgkallday98@gmail.com>
2025-11-23 23:10:36 -0500

    updated loginjs

--- assets/js/loginScript.js
+++ assets/js/loginScript.js
@@ -1,18 +1,3 @@
-// window.addEventListener("DOMContentLoaded", function(event) {
-// 	let login = document.getElementById('logLink');
-// 	let form = document.getElementById('loginModel');
-
-// 	document.getElementById('closeLogin').addEventListener('click', function(e) {
-// 		document.getElementById('loginModel').style.display = 'none';
-// 	})
-	
-// 	login.addEventListener('click', function(e) {
-// 		document.getElementById('loginModel').style.display = 'inline-block';
-// 	});
-// });
-
-
-
 window.addEventListener("DOMContentLoaded", function(event) {
     let login = document.getElementById('logLink');
     let form = document.getElementById('loginModel');
