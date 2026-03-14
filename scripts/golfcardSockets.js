e83cf9863d55bae4aa40704489af31c4cb78ffa5
Kevin H <dgkallday98@gmail.com>
2026-03-14 01:43:21 -0400

    updated golfSockets to update db as well as other connections

--- scripts/golfcardSockets.js
+++ scripts/golfcardSockets.js
@@ -1,3 +1,7 @@
+var {Course, Teebox, Round, Hole, RoundParticipant, Score} = require('../models/golfcard.js');
+var User = require('../models/user.js');
+const { QueryTypes } = require('sequelize');
+
 module.exports = (io) => {
 	io.on('connection', (socket) => {
 		console.log('New Client Connected');
@@ -11,12 +15,48 @@ module.exports = (io) => {
 			io.to(`round-${roundID}`).emit('roundStarted', {roundID});
 		});
 
-	  	socket.on('holeFinished', ({ roundID, userID, holeNum, strokes }) => {
-	    	console.log(`[server] holeFinished: user ${userID}, hole ${holeNum}, strokes: ${strokes}`);
-	    	console.log('update should fire');
-	   		 io.to(`round-${roundID}`).emit('updateScore', { userID, holeNum, strokes });
-	    	console.log('after update');
-	 	 });
+	  	socket.on('holeFinished', async ({ roundID, userID, holeNumber, strokes, fairwayHit, greenInReg }) => {
+	    	// console.log(`[server] holeFinished: user ${userID}, hole ${holeNum}, strokes: ${strokes}`);
+	    	// console.log('update should fire');
+	  		try {
+	  			await Score.create({
+	  				roundID: roundID,
+	  				userID: userID,
+	  				holeNumber: holeNumber,
+	  				strokes: strokes,
+	  				fairwayHit: fairwayHit,
+	  				greenInReg: greenInReg
+	  			});
+
+
+	  			// await sequelize.query(`
+	  			// 	INSERT INTO Scores (roundID, userID, holeNum, strokes, fairwayHit, greenInReg)
+	  			// 	VALUES (?, ?, ?, ?)
+	  			// 	ON DUPLICATE KEY UPDATE strokes = ?
+	  			// 	`, [roundID, userID, holeNum, strokes, fairwayHit, greenInReg, strokes]);
+
+	   			io.to(`round-${roundID}`).emit('updatedScore', { roundID });
+	  		} catch (err) {
+	  			console.error(err);
+	  		}
+	 	});
+
+	  	//when phone reconnects, refresh score.
+	  	socket.on("connect", () => {
+	  		console.log("connected");
+	  		fetch(`/rounds/${roundID}/scores`)
+	  			.then(res => res.json())
+	  			.then(scores => {
+	  				updateScoreboard(scores);
+	  			});
+	  	});
+
+	  	socket.on("reconnect", () => {
+  			console.log("socket reconnected");
+  			fetch(`/rounds/${roundID}/scores`)
+    			.then(res => res.json())
+    			.then(scores => updateScoreboard(scores));
+		});
 
 		socket.on('disconnect', () => {
 			console.log('client disconnected.');
